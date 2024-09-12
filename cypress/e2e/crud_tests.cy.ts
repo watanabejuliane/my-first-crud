a6c062f9ff40a0a38ffac6dbd39b8d254f78f54b
Juliane Watanabe B. Mendes <157089261+watanabejuliane@users.noreply.github.com>
2024-09-12 14:50:35 +0900

    testes do cypress concluidos

--- cypress/e2e/crud_tests.cy.ts
+++ cypress/e2e/crud_tests.cy.ts
@@ -1,22 +1,24 @@
 describe('CRUD Operations', () => {
   beforeEach(() => {
     // Visita a página principal antes de cada teste e limpa o estado
-    cy.visit('http://localhost:3000');  // Verifique se o servidor está rodando nesta URL
+    cy.visit('http://localhost:3000'); // Verifique se o servidor está rodando nesta URL
 
     // Limpa todos os itens existentes antes de cada teste
-    cy.get('#itemsList').then(($list) => {
-      if ($list.children().length > 0) {
-        $list.children().each(($el) => {
-          cy.wrap($el).find('button').contains('Delete').click();
-        });
-      }
+    cy.get('#itemsList').within(() => {
+      cy.get('li').each(($el) => {
+        cy.wrap($el).find('button').contains('Delete').click();
+      });
     });
   });
 
   it('should add a new item', () => {
     cy.get('input#name').should('be.visible').type('Novo Item');
     cy.get('button[type="submit"]').contains('Add').click();
-    cy.get('#itemsList').contains('Novo Item').should('exist');
+    
+    // Usa `within` para procurar dentro de `#itemsList` o item recém-adicionado
+    cy.get('#itemsList').within(() => {
+      cy.contains('li', 'Novo Item').should('exist');
+    });
   });
 
   it('should edit an existing item', () => {
@@ -24,15 +26,19 @@ describe('CRUD Operations', () => {
     cy.get('input#name').should('be.visible').type('Item para Editar');
     cy.get('button[type="submit"]').contains('Add').click();
 
-    // Aguarda o item ser adicionado e então o botão "Edit" deve aparecer
-    cy.get('#itemsList').contains('Item para Editar').parent().find('button').contains('Edit').should('be.visible').click();
+    // Usa `within` para encontrar o item e o botão "Edit"
+    cy.get('#itemsList').within(() => {
+      cy.contains('li', 'Item para Editar').find('button').contains('Edit').click();
+    });
 
-    // Aguarda o formulário de edição estar visível
+    // Aguarda o formulário de edição estar visível e edita o item
     cy.get('input#edit-name').should('be.visible').clear().type('Item Editado');
     cy.get('button[type="submit"]').contains('Update').click();
 
     // Verifica se o item foi editado
-    cy.get('#itemsList').contains('Item Editado').should('exist');
+    cy.get('#itemsList').within(() => {
+      cy.contains('li', 'Item Editado').should('exist');
+    });
   });
 
   it('should delete an item', () => {
@@ -40,10 +46,14 @@ describe('CRUD Operations', () => {
     cy.get('input#name').should('be.visible').type('Item para Deletar');
     cy.get('button[type="submit"]').contains('Add').click();
 
-    // Aguarda o item ser adicionado e então o botão "Delete" deve aparecer
-    cy.get('#itemsList').contains('Item para Deletar').parent().find('button').contains('Delete').should('be.visible').click();
+    // Usa `within` para encontrar o item e o botão "Delete"
+    cy.get('#itemsList').within(() => {
+      cy.contains('li', 'Item para Deletar').find('button').contains('Delete').click();
+    });
 
     // Verifica se o item foi removido
-    cy.get('#itemsList').contains('Item para Deletar').should('not.exist');
+    cy.get('#itemsList').within(() => {
+      cy.contains('li', 'Item para Deletar').should('not.exist');
+    });
   });
 });
